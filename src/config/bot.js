6cf5c7f7012c13d5aef3b2094d381fb52d3c4f20
iamthelion001 <fabriziogonzalezsipos92@gmail.com>
2026-06-23 15:51:42 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Quality service, instant delivery, satisfied customers.",
+        name: "🎬 Premium Entertainment",
         // Activity type number (0 = Playing).
         type: 0,
       },
